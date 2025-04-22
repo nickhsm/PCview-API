6d8a921e38886c59be189f1def068f94fce6853a
nickhsm <r0938000@student.thomasmore.be>
2025-04-22 15:11:27 +0200

    CORS added and env file parser

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import fastapi
 import base64
 import detector
+import config
+from fastapi.middleware.cors import CORSMiddleware
 
 from models.post_upload_image import Image
 
 
-app = fastapi.FastAPI(docs_url=None)
+app = fastapi.FastAPI(docs_url=config.documentation_url)
 
+origins = config.cors_origins.split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/upload")
 def query_image(image: Image):
+    """
+    Give it a base64 encoded image and this function will return the following (example):
+    {
+        "result": {
+            "class": "CPU",
+            "score": 99.9945297241211
+        }
+    }
+    """
     base64_string = image.image
 
     image_blob = base64.b64decode(base64_string)
