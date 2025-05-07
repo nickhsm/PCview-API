0af88d98bfa3c2ac14332f1ed2d0291d4fee2bfc
nickhsm <r0938000@student.thomasmore.be>
2025-05-07 12:16:15 +0200

    /quiz herschreven

--- queries/quiz_explanation.py
+++ queries/quiz_explanation.py
@@ -1,1 +1,3 @@
 quiz_explanation_query = 'SELECT "uitlegComponent" FROM "PCviewdb"."UitlegComponent" WHERE "componentNaam" = %s'
+
+quiz_query = 'SELECT v."vraag", v."uitlegWaaromGoed", a."correct", a."antwoord" FROM "PCviewdb"."Vraag" v JOIN "PCviewdb"."Antwoord" a ON a."vraagId" = v."vraagId" JOIN "PCviewdb"."Categorie" c ON c."categorieId" = v."categorieId" WHERE c."naam" = ''%s'''
